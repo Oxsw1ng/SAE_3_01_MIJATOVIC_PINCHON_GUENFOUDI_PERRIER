6a53c40cd973fa8e04748028a6b3d7ca0215e547
Yann Mijatovic <yannmijatovicp@gmail.com>
2022-12-15 17:49:49 +0100

    suite création

---
+++ Sources/main/java/mvc/sae_3_01_mijatovic_pinchon_guenfoudi_perrier/HelloController.java
@@ -0,0 +1,14 @@
+package mvc.sae_3_01_mijatovic_pinchon_guenfoudi_perrier;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+
+public class HelloController {
+    @FXML
+    private Label welcomeText;
+
+    @FXML
+    protected void onHelloButtonClick() {
+        welcomeText.setText("Welcome to JavaFX Application!");
+    }
+}
