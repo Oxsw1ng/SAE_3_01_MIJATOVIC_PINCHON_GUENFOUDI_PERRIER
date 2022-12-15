6eb361bfebbbccebcfbfb823ccd63f0650f1150b
Yann Mijatovic <yannmijatovicp@gmail.com>
2022-12-15 17:46:23 +0100

    Création du projet

---
+++ src/main/java/module-info.java
@@ -0,0 +1,8 @@
+module mvc.sae_3_01_mijatovic_pinchon_guenfoudi_perrier {
+    requires javafx.controls;
+    requires javafx.fxml;
+
+
+    opens mvc.sae_3_01_mijatovic_pinchon_guenfoudi_perrier to javafx.fxml;
+    exports mvc.sae_3_01_mijatovic_pinchon_guenfoudi_perrier;
+}
