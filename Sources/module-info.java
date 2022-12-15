90c665f3a62eadb83c1cd776605522c82233098c
Theo Pinchon <theo036188@gmail.com>
2022-12-15 18:13:46 +0100

    restructuration

--- Sources/module-info.java
+++ Sources/module-info.java
@@ -3,8 +3,6 @@ module mvc.sae_3_01_mijatovic_pinchon_guenfoudi_perrier {
     requires javafx.fxml;
 
 
-    opens mvc.sae_3_01_mijatovic_pinchon_guenfoudi_perrier to javafx.fxml;
-    exports mvc.sae_3_01_mijatovic_pinchon_guenfoudi_perrier;
     exports main.java.mvc.sae_3_01_mijatovic_pinchon_guenfoudi_perrier;
     opens main.java.mvc.sae_3_01_mijatovic_pinchon_guenfoudi_perrier to javafx.fxml;
 }
