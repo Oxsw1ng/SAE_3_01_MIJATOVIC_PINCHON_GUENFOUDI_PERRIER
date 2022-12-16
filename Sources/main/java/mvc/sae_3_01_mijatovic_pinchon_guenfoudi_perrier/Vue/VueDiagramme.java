7c2370df32fb09750bc592cb9dcab1b7e7c2d925
Theo Pinchon <theo036188@gmail.com>
2022-12-16 16:06:57 +0100

    mise a jour

--- Sources/main/java/mvc/sae_3_01_mijatovic_pinchon_guenfoudi_perrier/Vue/VueDiagramme.java
+++ Sources/main/java/mvc/sae_3_01_mijatovic_pinchon_guenfoudi_perrier/Vue/VueDiagramme.java
@@ -1,12 +1,40 @@
 package mvc.sae_3_01_mijatovic_pinchon_guenfoudi_perrier.Vue;
 
+import javafx.scene.layout.Pane;
 import mvc.sae_3_01_mijatovic_pinchon_guenfoudi_perrier.interfacesETabstract.Observateur;
 import mvc.sae_3_01_mijatovic_pinchon_guenfoudi_perrier.interfacesETabstract.Sujet;
+import mvc.sae_3_01_mijatovic_pinchon_guenfoudi_perrier.model.Classe;
+import mvc.sae_3_01_mijatovic_pinchon_guenfoudi_perrier.model.Modele;
 
-public class VueDiagramme implements Observateur {
+import java.util.ArrayList;
+
+public class VueDiagramme extends Pane implements Observateur {
+
+    private Modele m;
+
+    public VueDiagramme(Sujet s){
+        this.m = (Modele)s;
+    }
 
     @Override
     public void actualiser() {
+        for (Classe c:m.getClasses()) {
+
+
+
+        }
+
+
+
+    }
+
+    private void flecheAttr(Classe source, Classe target){
+
+    }
+    private void flecheHeredite(Classe source, Classe target){
+
+    }
+    private void flecheImplements(Classe source, Classe target){
 
     }
 
