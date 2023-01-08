40e52452e12181dd66073ad35766beeda5d970c9
tricki-lucky7 <remi.perrier1@etu.univ-lorraine.fr>
2023-01-08 20:49:54 +0100

    réparation bug mineur (traitement du cas où les classes sont l'une au dessus de l'autre)

--- Sources/main/java/mvc/sae_3_01_mijatovic_pinchon_guenfoudi_perrier/Vue/VueDiagramme.java
+++ Sources/main/java/mvc/sae_3_01_mijatovic_pinchon_guenfoudi_perrier/Vue/VueDiagramme.java
@@ -76,16 +76,17 @@ public class VueDiagramme extends Pane implements Observateur {
 //        }
     }
 
-    public static boolean pointAppartientSegment(Point C, Point A, Point B, int cote) {
+    public static boolean pointAppartientSegment(Point C, Point A, Point B) {
         int[] vecteurAB = new int[] {(int) (B.getX()-A.getX()), (int) (B.getY()-A.getY())};
         int[] vecteurAC = new int[] {(int) (C.getX()-A.getX()), (int) (C.getY()-A.getY())};
         boolean appartient = false;
 
+        // on vérifie que les vecteurs sont colinéaires
         if (vecteurAB[0] * vecteurAC[1] == vecteurAB[1] * vecteurAC[0]) {
             int prodScalaireABAC = vecteurAB[0] * vecteurAC[0] + vecteurAB[1] * vecteurAC[1];
             int prodScalaireABAB = vecteurAB[0] * vecteurAB[0] + vecteurAB[1] * vecteurAB[1];
 
-            if (0 < prodScalaireABAC && prodScalaireABAC < prodScalaireABAB) {
+            if (0 <= prodScalaireABAC && prodScalaireABAC <= prodScalaireABAB) {
                 appartient = true;
             }
         }
@@ -110,7 +111,7 @@ public class VueDiagramme extends Pane implements Observateur {
         // Calcul de la pente et de l'ordonnée à l'origine de la droite reliant les deux classes
         double m1;
         if (centreTarget.getX() - centreSource.getX() == 0) {
-            m1 = 1000000000000L;
+            m1 = 10000000000L;
         } else {
             m1 = (centreTarget.getY() - centreSource.getY()) / (centreTarget.getX() - centreSource.getX());
         }
@@ -131,7 +132,7 @@ public class VueDiagramme extends Pane implements Observateur {
             // Calcul de la pente et de l'ordonnée à l'origine de la droite reliant deux sommets du rectangle qui modélise la classe
             double m2;
             if (extremite2.getX() - extremite1.getX() == 0) {
-                m2 = 1000000000000L;
+                m2 = 10000000000L;
             } else {
                 m2 = (extremite2.getY() - extremite1.getY()) / (extremite2.getX() - extremite1.getX());
             }
@@ -140,18 +141,26 @@ public class VueDiagramme extends Pane implements Observateur {
             // Si les pentes des deux droites sont différentes, elles s'intersectent en un point unique
             if (m1 != m2) {
                 int x = (int) Math.abs(Math.round((p2 - p1) / (m2 - m1)));
-                // on cherche des valeurs à plus ou moins 1 à cause des arrondis, qui peuvent générer des erreurs
-                if ((extremite1.getX()==extremite2.getX()) &&  x-1 <= extremite1.getX() && extremite1.getX() >= x+1) {
+                // on cherche des valeurs à plus ou moins 2 à cause des arrondis, qui peuvent générer des erreurs
+                if ((extremite1.getX() == extremite2.getX()) && x-2 <= extremite1.getX() && extremite1.getX() >= x+2) {
                     x = (int) extremite1.getX();
                 }
-                int y = (int) Math.abs(Math.round(m1 * x + p1));
+                int y;
+                if (centreSource.getX() == centreTarget.getX()) {
+                    y = (int) Math.abs(Math.round(m1 * x - p1));
+                } else {
+                    y = (int) Math.abs(Math.round(m1 * x + p1));
+                }
                 //pareil ici
-                if ((extremite1.getY()==extremite2.getY()) && y-1 <= extremite1.getY() && extremite1.getY() >= y+1) {
+                if ((extremite1.getY() == extremite2.getY()) && y-2 <= extremite1.getY() && extremite1.getY() >= y+2) {
                     y = (int) extremite1.getY();
                 }
                 Point I = new Point(x, y);
 
-                if (pointAppartientSegment(I,extremite1,extremite2,i)) {
+//                // ligne servant à des tests uniquement afin de visualiser les points d'intersections
+//                this.getChildren().add(new Line(I.getX(), I.getY(), I.getX()+10, I.getY()+10));
+
+                if (pointAppartientSegment(I,extremite1,extremite2)) {
                     pointsAppartenantAuxSegments.add(I);
                 }
             }
@@ -169,100 +178,63 @@ public class VueDiagramme extends Pane implements Observateur {
         return bord;
     }
 
-//    public void flecheAgreg(VueClasse clsSource, VueClasse clsTarget){
-//        // Création de la ligne reliant les deux points
-////        Point source = new Point((int) (clsSource.getLayoutX()+clsSource.getLargeurClasse()/2), (int) (clsSource.getLayoutY()+clsSource.getHauteurClasse()/2));
-////        Point target = new Point((int) (clsTarget.getLayoutX()+clsTarget.getLargeurClasse()/2), (int) (clsTarget.getLayoutY()+clsTarget.getHauteurClasse()/2));
-//
-//        Line ligne = new Line(source.getX(), source.getY(), target.getX(), target.getY());
-//
-//        // Calcul de l'angle de la ligne
-//        double angle = Math.atan2(target.getY() - source.getY(), target.getX() - source.getX());
-//
-//        // Calcul des coordonnées des points de la flèche
-//        double x3 = target.getX() - 20 * Math.cos(angle - Math.PI / 6);
-//        double y3 = target.getY() - 20 * Math.sin(angle - Math.PI / 6);
-//        double x4 = target.getX() - 20 * Math.cos(angle + Math.PI / 6);
-//        double y4 = target.getY() - 20 * Math.sin(angle + Math.PI / 6);
-//
-//        // Création de la flèche
-//        Polygon fleche = new Polygon();
-//        fleche.getPoints().addAll(
-//                target.getX(), target.getY(),
-//                x3, y3,
-//                x4, y4
-//        );
-//        fleche.setFill(Color.WHITE);
-//        fleche.setStroke(Color.BLACK);
-//        this.getChildren().addAll(ligne,fleche);
-
-//        PauseTransition pauseTransition = new PauseTransition(new Duration(3000.0));
-//        pauseTransition.setOnFinished(e -> this.pointBordClasse(source,target));//this.getChildren().add(new Line(pointBordClasse(source,target).getX(),pointBordClasse(source,target).getY(),pointBordClasse(target,source).getX(), pointBordClasse(target,source).getY())));
-//        pauseTransition.play();
-
-//        int[] depart = pointBordClasse(source,target);
-//        int[] arrive = pointBordClasse(target, source);
-//        this.getChildren().add(new Line(depart[0],depart[1], arrive[0], arrive[1]));
-
-
-//    }
-
     public void fleche(Point source, Point target, int type, String texteMilieuFleche){
-        // Création de la ligne reliant les deux points
-        Line ligne = new Line(source.getX(), source.getY(), target.getX(), target.getY());
-        ligne.setStroke(this.modele.getTheme().getBordureEtBtnImportant());
-        ligne.setStrokeWidth(2);
+        if (source != null || target != null) {
+            // Création de la ligne reliant les deux points
+            Line ligne = new Line(source.getX(), source.getY(), target.getX(), target.getY());
+            ligne.setStroke(this.modele.getTheme().getBordureEtBtnImportant());
+            ligne.setStrokeWidth(2);
 
-        this.getChildren().add(ligne);
+            this.getChildren().add(ligne);
 
-        // Calcul de l'angle de la ligne
-        double angle = Math.atan2(target.getY() - source.getY(), target.getX() - source.getX());
+            // Calcul de l'angle de la ligne
+            double angle = Math.atan2(target.getY() - source.getY(), target.getX() - source.getX());
 
-        // Calcul des coordonnées des points de la flèche
-        double x3 = target.getX() - 25 * Math.cos(angle - Math.PI / 6);
-        double y3 = target.getY() - 25 * Math.sin(angle - Math.PI / 6);
-        double x4 = target.getX() - 25 * Math.cos(angle + Math.PI / 6);
-        double y4 = target.getY() - 25 * Math.sin(angle + Math.PI / 6);
+            // Calcul des coordonnées des points de la flèche
+            double x3 = target.getX() - 25 * Math.cos(angle - Math.PI / 6);
+            double y3 = target.getY() - 25 * Math.sin(angle - Math.PI / 6);
+            double x4 = target.getX() - 25 * Math.cos(angle + Math.PI / 6);
+            double y4 = target.getY() - 25 * Math.sin(angle + Math.PI / 6);
 
-        switch (type) {
-            case FLECHE_IMPLEMENTATION :
-                ArrayList dash = new ArrayList<>();
-                dash.add(20.0);
-                dash.add(20.0);
-                ligne.getStrokeDashArray().addAll(dash);
-            case FLECHE_HEREDITE :
-                // Création de la flèche
-                Polygon fleche = new Polygon();
-                fleche.getPoints().addAll(
-                        target.getX(), target.getY(),
-                        x3, y3,
-                        x4, y4
-                );
-                fleche.setFill(this.modele.getTheme().getFondNavEtArbo());
-                fleche.setStroke(this.modele.getTheme().getBordureEtBtnImportant());
-                fleche.setStrokeWidth(2);
-                this.getChildren().add(fleche);
-                break;
-            case FLECHE_AGREGATION :
+            switch (type) {
+                case FLECHE_IMPLEMENTATION:
+                    ArrayList dash = new ArrayList<>();
+                    dash.add(20.0);
+                    dash.add(20.0);
+                    ligne.getStrokeDashArray().addAll(dash);
+                case FLECHE_HEREDITE:
+                    // Création de la flèche
+                    Polygon fleche = new Polygon();
+                    fleche.getPoints().addAll(
+                            target.getX(), target.getY(),
+                            x3, y3,
+                            x4, y4
+                    );
+                    fleche.setFill(this.modele.getTheme().getFondNavEtArbo());
+                    fleche.setStroke(this.modele.getTheme().getBordureEtBtnImportant());
+                    fleche.setStrokeWidth(2);
+                    this.getChildren().add(fleche);
+                    break;
+                case FLECHE_AGREGATION:
 
-                Line ligne2 = new Line(x3,y3, target.getX(), target.getY());
-                ligne2.setStroke(this.modele.getTheme().getBordureEtBtnImportant());
-                ligne2.setStrokeWidth(2);
+                    Line ligne2 = new Line(x3, y3, target.getX(), target.getY());
+                    ligne2.setStroke(this.modele.getTheme().getBordureEtBtnImportant());
+                    ligne2.setStrokeWidth(2);
 
-                Line ligne3 = new Line(x4,y4, target.getX(), target.getY());
-                ligne3.setStroke(this.modele.getTheme().getBordureEtBtnImportant());
-                ligne3.setStrokeWidth(2);
+                    Line ligne3 = new Line(x4, y4, target.getX(), target.getY());
+                    ligne3.setStroke(this.modele.getTheme().getBordureEtBtnImportant());
+                    ligne3.setStrokeWidth(2);
 
-                javafx.scene.control.Label labelAgregation = new javafx.scene.control.Label(texteMilieuFleche);
-                double x = (ligne.getStartX() + ligne.getEndX()) / 2;
-                double y = (ligne.getStartY() + ligne.getEndY()) / 2;
-                labelAgregation.setLayoutX(x - labelAgregation.getWidth() / 2);
-                labelAgregation.setLayoutY(y - labelAgregation.getHeight() / 2);
+                    javafx.scene.control.Label labelAgregation = new javafx.scene.control.Label(texteMilieuFleche);
+                    double x = (ligne.getStartX() + ligne.getEndX()) / 2;
+                    double y = (ligne.getStartY() + ligne.getEndY()) / 2;
+                    labelAgregation.setLayoutX(x - labelAgregation.getWidth() / 2);
+                    labelAgregation.setLayoutY(y - labelAgregation.getHeight() / 2);
 
-                this.getChildren().addAll(ligne2, ligne3, labelAgregation);
-                break;
+                    this.getChildren().addAll(ligne2, ligne3, labelAgregation);
+                    break;
+            }
         }
-
     }
 
     public void flecheImplements(Point source, Point target){
