02928f08a3815e3d3212ac260ed92527c91a9156
NaelGuenfoudi <guenfmen@gmail.com>
2023-01-09 09:29:54 +0100

    ajout d'un scenario et d'un diagramme de sequence

--- Sources/main/java/mvc/sae_3_01_mijatovic_pinchon_guenfoudi_perrier/controller/ControllerDragExterieur.java
+++ Sources/main/java/mvc/sae_3_01_mijatovic_pinchon_guenfoudi_perrier/controller/ControllerDragExterieur.java
@@ -17,7 +17,7 @@ public class ControllerDragExterieur implements EventHandler<DragEvent> {
 
     @Override
     public void handle(DragEvent dragEvent) {
-        File fichierClasse=handleDragDroppedEvent(dragEvent);
+        File fichierClasse=recupererFichierDepose(dragEvent);
         if(fichierClasse!=null){
             String pathClasse=fichierClasse.getPath();
             Classe nouvelleClasse=new Classe(pathClasse,modele);
@@ -31,7 +31,7 @@ public class ControllerDragExterieur implements EventHandler<DragEvent> {
      * @param event l'événement de glisser-déposer
      * @return le fichier .class déposé, ou null si aucun fichier .class n'a été déposé
      */
-    private File handleDragDroppedEvent(DragEvent event) {
+    private File recupererFichierDepose(DragEvent event) {
         Dragboard db = event.getDragboard();
         File classFile = null;
         if (db.hasFiles()) {
