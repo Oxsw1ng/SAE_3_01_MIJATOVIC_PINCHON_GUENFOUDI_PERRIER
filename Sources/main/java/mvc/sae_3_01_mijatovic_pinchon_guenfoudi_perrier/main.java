a72ed1034a379dbc1458a442ab393c6812c7a505
Yann Mijatovic <yannmijatovicp@gmail.com>
2023-01-10 17:36:44 +0100

    Ajout bilan It 5

--- Sources/main/java/mvc/sae_3_01_mijatovic_pinchon_guenfoudi_perrier/main.java
+++ Sources/main/java/mvc/sae_3_01_mijatovic_pinchon_guenfoudi_perrier/main.java
@@ -1,16 +1,24 @@
 package mvc.sae_3_01_mijatovic_pinchon_guenfoudi_perrier;
 
 import javafx.application.Application;
+import javafx.event.EventHandler;
 import javafx.geometry.Insets;
 import javafx.scene.Scene;
+import javafx.scene.control.ScrollPane;
+import javafx.scene.input.KeyEvent;
+import javafx.scene.input.ZoomEvent;
 import javafx.scene.layout.*;
+import javafx.scene.transform.Scale;
 import javafx.stage.Stage;
 import mvc.sae_3_01_mijatovic_pinchon_guenfoudi_perrier.Vue.*;
 import mvc.sae_3_01_mijatovic_pinchon_guenfoudi_perrier.model.Modele;
 
+import java.util.concurrent.atomic.AtomicReference;
+
 public class main extends Application {
     public static double SIZEX = 0;
-    public static  double SIZEY = 0;
+    public static double SIZEY = 0;
+
     @Override
     public void start(Stage stage) throws Exception {
 
@@ -27,19 +35,20 @@ public class main extends Application {
         // explorateur de fichier a gauche
         VueRepCourant vueRepCourant = new VueRepCourant(modele);
         borderPane.setLeft(vueRepCourant);
-        BorderPane.setMargin(vueRepCourant,new Insets(0,0,5,3));
+        BorderPane.setMargin(vueRepCourant, new Insets(0, 0, 5, 3));
         vueRepCourant.actualiser();
         modele.enregistrerObservateur(vueRepCourant);
 
         // panneau central de l'application
         VueDiagramme paneCenter = new VueDiagramme(modele);
+        // TEST TEST TEST
         borderPane.setCenter(paneCenter);
         paneCenter.actualiser();
 
         modele.enregistrerObservateur(paneCenter);
         modele.enregistrerObservateur(borderPane);
 
-        Scene scene = new Scene(borderPane,1000,800);
+        Scene scene = new Scene(borderPane, 1000, 800);
         stage.setScene(scene);
         stage.setTitle("DiagMov'");
         stage.setMinWidth(310);
