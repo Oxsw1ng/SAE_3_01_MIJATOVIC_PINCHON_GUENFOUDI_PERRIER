fa7c1f21e79508867f1d32a484f2ade7c338999e
NaelGuenfoudi <guenfmen@gmail.com>
2023-01-12 16:07:53 +0100

    interface pour gerer le resizing et le deplacement

---
+++ Sources/main/java/mvc/sae_3_01_mijatovic_pinchon_guenfoudi_perrier/controller/DragController.java
@@ -0,0 +1,8 @@
+package mvc.sae_3_01_mijatovic_pinchon_guenfoudi_perrier.controller;
+
+import javafx.scene.input.DragEvent;
+import javafx.scene.input.MouseEvent;
+
+public interface DragController {
+    public void handleDrag(MouseEvent mouseEvent);
+}
